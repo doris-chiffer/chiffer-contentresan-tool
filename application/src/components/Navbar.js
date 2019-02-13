299ee73007f31ac1b04c3bf417598b017318fdad
Doris <doris.obor@chiffer.se>
2019-02-13 16:49:03 +0100

    updated navbar

--- application/src/components/Navbar.js
+++ application/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Collapse,
   Navbar,
@@ -10,20 +10,21 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  DropdownItem,
+} from 'reactstrap';
 
-export default class Example extends React.Component {
+class ToolNavbar extends Component {
   constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false
+      isOpen: false,
     };
   }
   toggle() {
     this.setState({
-      isOpen: !this.state.isOpen
+      isOpen: !this.state.isOpen,
     });
   }
   render() {
@@ -34,29 +35,26 @@ export default class Example extends React.Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                  Options
+                  LÄGG TILL STEG
                 </DropdownToggle>
                 <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
                   <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
+                  <DropdownItem>Reset</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href="/">SPARA</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">SKRIV UT</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">HJÄLP</NavLink>
+              </NavItem>
             </Nav>
           </Collapse>
         </Navbar>
@@ -64,3 +62,5 @@ export default class Example extends React.Component {
     );
   }
 }
+
+export default ToolNavbar;
