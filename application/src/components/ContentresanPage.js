73fb1e80089592940a89b96424f6792c1a4a147c
Doris <doris.obor@chiffer.se>
2019-04-09 11:55:29 +0200

    commented out the delete functions and buttons

--- application/src/components/ContentresanPage.js
+++ application/src/components/ContentresanPage.js
@@ -32,11 +32,24 @@ class ContentResanPage extends Component {
     event.preventDefault();
     const input = document.getElementById('div-to-pdf');
     html2canvas(input).then(canvas => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('landscape');
-      // const width = pdf.internal.pageSize.getWidth();
-      // const height = pdf.internal.pageSize.getHeight();
-      pdf.addImage(imgData, 'PNG', 10, 10, 280, 235);
+      const largerScreen = window.matchMedia('(min-width:2600px)');
+
+      const imgData = canvas.toDataURL('image/png', 1.0); //image format
+      const pdf = new jsPDF('landscape', 'mm', 'a4'); // landscape size page of PDF
+      //  const width = pdf.internal.pageSize.getWidth();
+      //  const height = pdf.internal.pageSize.getHeight();
+
+      if (largerScreen.matches) {
+        pdf.addImage(imgData, 'PNG', -100, 10, 500, 200); // image, format, x-axis, y-axis, width, height
+      } else if (window.matchMedia('(min-width:2000px)').matches) {
+        pdf.addImage(imgData, 'PNG', 0, 10, 300, 200); // image, format, x-axis, y-axis, width, height
+      } else if (window.matchMedia('(min-width:1024px)').matches) {
+        pdf.addImage(imgData, 'PNG', 50, 10, 190, 200); // image, format, x-axis, y-axis, width, height
+      } else {
+        // image, format, x-axis, y-axis, width, height
+        pdf.addImage(imgData, 'PNG', 60, 10, 150, 200);
+      }
+
       pdf.save(`${this.props.projectName}.pdf`);
     });
   };
