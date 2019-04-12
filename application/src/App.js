d56a0aeb0e9f40634568a07447c410c14e5bb246
Doris <doris.obor@chiffer.se>
2019-04-12 15:53:13 +0200

    added loading page

--- application/src/App.js
+++ application/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './sass/App.scss';
 import './sass/ContentresanPage.scss';
 import './sass/ContentresanCircle.scss';
+import loadingSpinner from './images/loading-spinner.gif';
 
 //components
 import StartPage from './components/StartPage';
@@ -13,13 +14,57 @@ class App extends Component {
   state = {
     loggedIn: false,
     projectName: '',
+    loading: 'initial',
+    data: '',
   };
 
+  loadData() {
+    var promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // console.log('wait 1 second');
+        resolve('This is the data.');
+      }, 1000);
+    });
+    return promise;
+  }
+  componentDidMount() {
+    this.setState({ loading: 'true' });
+    this.loadData().then(data => {
+      this.setState({
+        data: data,
+        loading: 'false',
+      });
+    });
+  }
   handleLogin = projectName => {
     this.setState({ loggedIn: true, projectName: projectName });
   };
 
   render() {
+    /*This happens after the class is constructed. You will not 
+    see this element because React is still computing changes to the DOM.*/
+
+    if (this.state.loading === 'initial') {
+      return (
+        <div className="loading-page">
+          <div className="loading-spinner">
+            <img src={loadingSpinner} alt="Loading.." />
+          </div>
+        </div>
+      );
+    }
+
+    //This happens when we wait for data.
+    if (this.state.loading === 'true') {
+      return (
+        <div className="loading-page">
+          <div className="loading-spinner">
+            <img src={loadingSpinner} alt="Loading.." />
+          </div>
+        </div>
+      );
+    }
+
     if (this.state.loggedIn) {
       return (
         <div className="page-container">
