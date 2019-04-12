782801ac108e15494147296e4eeb203857a534ac
Doris <doris.obor@chiffer.se>
2019-04-12 19:00:35 +0200

    Updates

---
+++ precache-manifest.70ef548895573d254aab80fa6b9725a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdc148390becc83eb4c981594e9d13f4",
+    "url": "/static/media/startpage-background.fdc14839.jpg"
+  },
+  {
+    "revision": "aeec07c40441ed2743013207b2733065",
+    "url": "/static/media/second-background.aeec07c4.png"
+  },
+  {
+    "revision": "15be5601d934d96a7a06951c0bca285b",
+    "url": "/static/media/loading-spinner.15be5601.gif"
+  },
+  {
+    "revision": "8774e17cae77d022b2676db19148f0da",
+    "url": "/static/media/contentresan-tutorial.8774e17c.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bfa2cbc384c8f7329d67",
+    "url": "/static/js/main.bfa2cbc3.chunk.js"
+  },
+  {
+    "revision": "25f7d89dc52ae9d87fce",
+    "url": "/static/js/2.25f7d89d.chunk.js"
+  },
+  {
+    "revision": "bfa2cbc384c8f7329d67",
+    "url": "/static/css/main.df985660.chunk.css"
+  },
+  {
+    "revision": "25f7d89dc52ae9d87fce",
+    "url": "/static/css/2.94031de9.chunk.css"
+  },
+  {
+    "revision": "03b9ed8a3fdf1d565eeca8ead77d7bd9",
+    "url": "/index.html"
+  }
+];
